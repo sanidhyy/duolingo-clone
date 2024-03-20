aefccc537382ca52805cc604792624397208974d
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-20 10:14:38 +0000

    drizzle schema added

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,7 +1,9 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 
+import * as schema from "./schema";
+
 const sql = neon(process.env.DATABASE_URL!);
-const db = drizzle(sql);
+const db = drizzle(sql, { schema });
 
 export default db;
---
+++ db/schema.ts
@@ -0,0 +1,7 @@
+import { pgTable, serial, text } from "drizzle-orm/pg-core";
+
+export const courses = pgTable("courses", {
+  id: serial("id").primaryKey(),
+  title: text("title").notNull(),
+  imageSrc: text("image_src").notNull(),
+});
