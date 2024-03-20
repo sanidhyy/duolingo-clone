4554e6350365265f1cd909ea3664f33678c26aca
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-20 10:07:39 +0000

    drizzle db connection created

---
+++ db/drizzle.ts
@@ -0,0 +1,7 @@
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+
+const sql = neon(process.env.DATABASE_URL!);
+const db = drizzle(sql);
+
+export default db;
