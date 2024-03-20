8ccc2f77cd2c0ef01f2dcd0fb33fa048f0d4ad2e
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-20 08:13:21 +0000

    custom variants added

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -13,12 +13,15 @@ const buttonVariants = cva(
           "bg-white text-black border-slate-200 border-2 border-b-4 active:border-b-2 hover:bg-slate-100 text-slate-500",
 
         // custom
+        locked:
+          "bg-neutral-200 text-primary-foreground hover:bg-neutral-200/90 border-neutral-400 border-b-4 active:border-b-0",
+
         primary:
           "bg-sky-400 text-primary-foreground hover:bg-sky-400/90 border-sky-500 border-b-4 active:border-b-0",
         primaryOutline: "bg-white text-sky-500 hover:bg-slate-100",
 
         secondary:
-          "bg-green-400 text-primary-foreground hover:bg-green-500/90 border-green-600 border-b-4 active:border-b-0",
+          "bg-green-500 text-primary-foreground hover:bg-green-500/90 border-green-600 border-b-4 active:border-b-0",
         secondaryOutline: "bg-white text-green-500 hover:bg-slate-100",
 
         danger:
@@ -34,7 +37,6 @@ const buttonVariants = cva(
 
         sidebar:
           "bg-transparent text-slate-500 border-2 border-transparent hover:bg-slate-100 transition-none",
-
         sidebarOutline:
           "bg-sky-500/15 text-sky-500 border-sky-300 border-2 hover:bg-sky-500/20 transition-none",
       },
