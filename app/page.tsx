eebd35f9639bed8c7b2b660f0fea39677db28d3c
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-20 06:37:32 +0000

    button component tested

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className="">
       <p className="text-green-500 font-bold text-7xl">Hello Lingo!</p>
+
+      <Button variant="secondary" size="lg">
+        Click Me
+      </Button>
     </div>
   );
 }
