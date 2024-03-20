4e292bdf4f429ef39a99efe7528cf9525e11fd97
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-20 08:51:30 +0000

    mobile header color fix

--- components/mobile-header.tsx
+++ components/mobile-header.tsx
@@ -2,7 +2,7 @@ import { MobileSidebar } from "./mobile-sidebar";
 
 export const MobileHeader = () => {
   return (
-    <nav className="lg:hidden px-4 h-[50px] flex items-center bg-yellow-500 border-b fixed top-0 w-full z-50">
+    <nav className="lg:hidden px-4 h-[50px] flex items-center bg-green-500 border-b fixed top-0 w-full z-50">
       <MobileSidebar />
     </nav>
   );
