2ea935137002a2fd01d5549486e6b41b9800de99
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-20 15:47:38 +0000

    get course by id query added

--- db/queries.ts
+++ db/queries.ts
@@ -3,7 +3,7 @@ import { eq } from "drizzle-orm";
 import { cache } from "react";
 
 import db from "./drizzle";
-import { userProgress } from "./schema";
+import { courses, userProgress } from "./schema";
 
 export const getCourses = cache(async () => {
   const data = await db.query.courses.findMany();
@@ -21,6 +21,15 @@ export const getUserProgress = cache(async () => {
     with: {
       activeCourse: true,
     },
+  });
+
+  return data;
+});
+
+export const getCourseById = cache(async (courseId: number) => {
+  const data = await db.query.courses.findFirst({
+    where: eq(courses.id, courseId),
+    // TODO: Populate units and lessons
   });
 
   return data;
