88a182b8056b650115c4c914c1e189e56cdeb5a3
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-20 11:19:50 +0000

    get user progress query created

--- db/queries.ts
+++ db/queries.ts
@@ -1,9 +1,27 @@
+import { auth } from "@clerk/nextjs";
+import { eq } from "drizzle-orm";
 import { cache } from "react";
 
 import db from "./drizzle";
+import { userProgress } from "./schema";
 
 export const getCourses = cache(async () => {
   const data = await db.query.courses.findMany();
+
+  return data;
+});
+
+export const getUserProgress = cache(async () => {
+  const { userId } = auth();
+
+  if (!userId) return null;
+
+  const data = await db.query.userProgress.findFirst({
+    where: eq(userProgress.userId, userId),
+    with: {
+      activeCourse: true,
+    },
+  });
 
   return data;
 });
