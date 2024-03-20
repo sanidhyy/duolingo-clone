2bd589771908b63f31591483ca7a4d0cf2b62bbd
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-20 10:14:44 +0000

    drizzle config created

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import "dotenv/config";
+import type { Config } from "drizzle-kit";
+
+export default {
+  schema: "./db/schema.ts",
+  out: "./drizzle",
+  driver: "pg",
+  dbCredentials: {
+    connectionString: process.env.DATABASE_URL!,
+  },
+} satisfies Config;
