89204fbea41a9b472f188fc068713a822b5b953b
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-20 09:59:26 +0000

    fixed main header position

--- app/(main)/learn/header.tsx
+++ app/(main)/learn/header.tsx
@@ -9,7 +9,7 @@ type HeaderProps = {
 
 export const Header = ({ title }: HeaderProps) => {
   return (
-    <div className="sticky top-0 bg-white pb-3 lg:pt-[28px] flex items-center justify-between border-b-2 mb-5 text-neutral-400 lg:z-50">
+    <div className="sticky top-0 bg-white pb-3 lg:pt-[28px] lg:mt-[-28px] flex items-center justify-between border-b-2 mb-5 text-neutral-400 lg:z-50">
       <Link href="">
         <Button size="sm" variant="ghost">
           <ArrowLeft className="h-5 w-5 stroke-2 text-neutral-400" />
