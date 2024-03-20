8aadea7eacfececb1520623aa825a8296968dcba
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-20 07:52:02 +0000

    clerk setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
+import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
+
 import "./globals.css";
 
 const font = Nunito({ subsets: ["latin"] });
@@ -15,8 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={font.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
