bfa789aa8cf9acf63bb5c48195de404244b3fa22
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-21 10:52:29 +0000

    seed script updated

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -191,6 +191,31 @@ const main = async () => {
       },
     ]);
 
+    console.log("Inserting challenge for Spanish (Unit 1 Lesson 2)...");
+    await db.insert(schema.challenges).values([
+      {
+        id: 4,
+        lessonId: 2, // Verbs
+        type: "SELECT",
+        order: 1,
+        question: 'Which one of these is the "the man"?',
+      },
+      {
+        id: 5,
+        lessonId: 2, // Verbs
+        type: "ASSIST",
+        order: 2,
+        question: '"the man"',
+      },
+      {
+        id: 6,
+        lessonId: 2, // Verbs
+        type: "SELECT",
+        order: 3,
+        question: 'Which one of these is "the robot"?',
+      },
+    ]);
+
     console.log("Seeding finished.");
   } catch (error: unknown) {
     console.error(error);
