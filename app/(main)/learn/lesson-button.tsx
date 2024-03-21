762f3ea53ffd771c7d9835945d8eeab2563ac8d3
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-21 06:31:24 +0000

    accessibility fixes

--- app/(main)/learn/lesson-button.tsx
+++ app/(main)/learn/lesson-button.tsx
@@ -63,7 +63,10 @@ export const LessonButton = ({
           <div className="h-[102px] w-[102px] relative">
             <div className="absolute -top-6 left-2.5 px-3 py-2.5 border-2 font-bold uppercase text-green-500 bg-white rounded-xl animate-bounce tracking-wide z-10">
               Start
-              <div className="absolute left-1/2 -bottom-2 w-0 h-0 border-x-8 border-x-transparent border-t-8 transform -translate-x-1/2" />
+              <div
+                className="absolute left-1/2 -bottom-2 w-0 h-0 border-x-8 border-x-transparent border-t-8 transform -translate-x-1/2"
+                aria-hidden
+              />
             </div>
             <CircularProgressbarWithChildren
               value={Number.isNaN(percentage) ? 0 : percentage}
