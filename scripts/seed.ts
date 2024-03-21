b0c8978074688281c924c949387002e10db8e539
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-21 05:03:15 +0000

    seed script updated

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -62,6 +62,30 @@ const main = async () => {
         order: 1,
         title: "Nouns",
       },
+      {
+        id: 2,
+        unitId: 1, // Unit 1 (Learn the basics...)
+        order: 2,
+        title: "Verbs",
+      },
+      {
+        id: 3,
+        unitId: 1, // Unit 1 (Learn the basics...)
+        order: 3,
+        title: "Sentences",
+      },
+      {
+        id: 4,
+        unitId: 1, // Unit 1 (Learn the basics...)
+        order: 4,
+        title: "Pronouns",
+      },
+      {
+        id: 5,
+        unitId: 1, // Unit 1 (Learn the basics...)
+        order: 5,
+        title: "Revision",
+      },
     ]);
 
     console.log("Inserting challenge for Spanish (Unit 1 Lesson 1)...");
