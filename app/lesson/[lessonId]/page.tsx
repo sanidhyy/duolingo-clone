ec0ff70cd0f1f08e3ebb0745fb18d402f363d51d
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-21 12:02:53 +0000

    lesson practice page created

--- app/lesson/[lessonId]/page.tsx
+++ app/lesson/[lessonId]/page.tsx
@@ -2,10 +2,16 @@ import { redirect } from "next/navigation";
 
 import { getLesson, getUserProgress } from "@/db/queries";
 
-import { Quiz } from "./quiz";
+import { Quiz } from "../quiz";
 
-const LessonPage = async () => {
-  const lessonData = getLesson();
+type LessonIdPageProps = {
+  params: {
+    lessonId: number;
+  };
+};
+
+const LessonIdPage = async ({ params }: LessonIdPageProps) => {
+  const lessonData = getLesson(params.lessonId);
   const userProgressData = getUserProgress();
 
   const [lesson, userProgress] = await Promise.all([
@@ -31,4 +37,4 @@ const LessonPage = async () => {
   );
 };
 
-export default LessonPage;
+export default LessonIdPage;
