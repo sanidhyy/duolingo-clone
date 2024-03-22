e4009c803acf73b33b28c1a412ae7d8a0a439a24
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-22 08:15:58 +0000

    headers updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/api/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+          {
+            key: "Content-Range",
+            value: "bytes : 0-9/*",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
