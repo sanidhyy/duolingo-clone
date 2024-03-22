e6e6b73ad31a4f658eb6a28e9b152b1d1b8eb59c
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-22 08:16:14 +0000

    admin lib created

---
+++ lib/admin.ts
@@ -0,0 +1,10 @@
+import { auth } from "@clerk/nextjs";
+
+export const getIsAdmin = () => {
+  const { userId } = auth();
+  const adminIds = process.env.CLERK_ADMIN_IDS!.split(", "); // stored in .env.local file as string separated by comma(,) and space( )
+
+  if (!userId) return false;
+
+  return adminIds.indexOf(userId) !== -1;
+};
