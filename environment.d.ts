07b0155cf1a17f107cb4d54129696730d8fa0ab9
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-22 08:16:06 +0000

    env types added

--- environment.d.ts
+++ environment.d.ts
@@ -13,6 +13,9 @@ declare global {
 
       // public app url
       NEXT_PUBLIC_APP_URL: string;
+
+      // clerk admin user id(s) (separated by comma(,) and space( )). Ex: "user_123, user_456, user_789"
+      CLERK_ADMIN_IDS: string;
     }
   }
 }
