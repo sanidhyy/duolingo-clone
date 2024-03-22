e0c44622bc68f7267fbec007b555c261dc5541d1
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-22 08:59:22 +0000

    reset script created

---
+++ scripts/reset.ts
@@ -0,0 +1,30 @@
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+
+import * as schema from "../db/schema";
+
+const sql = neon(process.env.DATABASE_URL!);
+
+const db = drizzle(sql, { schema });
+
+const main = async () => {
+  try {
+    console.log("Resetting database...");
+    await db.delete(schema.courses);
+    await db.delete(schema.userProgress);
+    await db.delete(schema.units);
+    await db.delete(schema.lessons);
+    await db.delete(schema.challenges);
+    await db.delete(schema.challengeOptions);
+    await db.delete(schema.challengeProgress);
+    await db.delete(schema.userSubscription);
+
+    console.log("Resetting finished.");
+  } catch (error: unknown) {
+    console.error(error);
+    throw new Error("Failed to seed the database.");
+  }
+};
+
+main();
