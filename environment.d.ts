6998b4603070af10681397478144def829ce4e40
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-22 06:18:39 +0000

    env types added

--- environment.d.ts
+++ environment.d.ts
@@ -7,8 +7,9 @@ declare global {
       // neon db uri
       DATABASE_URL: string;
 
-      // stripe api key
+      // stripe api key and webhook
       STRIPE_API_SECRET_KEY: string;
+      STRIPE_WEBHOOK_SECRET: string;
 
       // public app url
       NEXT_PUBLIC_APP_URL: string;
