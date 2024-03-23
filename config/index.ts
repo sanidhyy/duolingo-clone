478fb387d83157449557f290f338e50cdfdcca51
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-23 04:22:45 +0000

    metadata added

--- config/index.ts
+++ config/index.ts
@@ -1,3 +1,50 @@
+import type { Metadata } from "next";
+
+export const siteConfig: Metadata = {
+  title: "Lingo",
+  description:
+    "Interactive platform for language learning with lessons, quizzes, and progress tracking.",
+  keywords: [
+    "reactjs",
+    "nextjs",
+    "vercel",
+    "react",
+    "duolingo-clone",
+    "learn-language",
+    "shadcn",
+    "shadcn-ui",
+    "radix-ui",
+    "cn",
+    "clsx",
+    "lingo",
+    "postgresql",
+    "sonner",
+    "drizzle",
+    "zustand",
+    "mysql",
+    "lucide-react",
+    "next-themes",
+    "clerk-themes",
+    "clerk",
+    "postcss",
+    "prettier",
+    "react-dom",
+    "tailwindcss",
+    "tailwindcss-animate",
+    "ui/ux",
+    "js",
+    "javascript",
+    "typescript",
+    "eslint",
+    "html",
+    "css",
+  ] as Array<string>,
+  authors: {
+    name: "Sanidhya Kumar Verma",
+    url: "https://github.com/sanidhyy",
+  },
+} as const;
+
 export const links = {
   sourceCode: "https://github.com/sanidhyy/duolingo-clone",
 } as const;
