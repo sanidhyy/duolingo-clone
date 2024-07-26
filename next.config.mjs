971891f8cc8ce9ecfbed9b1b22bb93778be6e472
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-07-26 19:31:12 +0530

    added all image sources

--- next.config.mjs
+++ next.config.mjs
@@ -25,6 +25,14 @@ const nextConfig = {
       },
     ];
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
