f8fdbcb6cedd94cb517b55e6fde33e40b2af11b9
Yoshi Rubin <yoshrubin@gmail.com>
2024-09-19 21:48:04 +0300

    replace logo

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -17,11 +17,7 @@ export const Header = () => {
     <header className="h-20 w-full border-b-2 border-slate-200 px-4">
       <div className="mx-auto flex h-full items-center justify-between lg:max-w-screen-lg">
         <Link href="/" className="flex items-center gap-x-3 pb-7 pl-4 pt-8">
-          <Image src="/mascot.svg" alt="Mascot" height={40} width={40} />
-
-          <h1 className="text-2xl font-extrabold tracking-wide text-green-600">
-            Lingo
-          </h1>
+          <Image src="/YIDIOS.png" alt="Logo" height={40} width={98} />
         </Link>
 
         <div className="flex gap-x-3">
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -21,11 +21,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
     >
       <Link href="/learn">
         <div className="flex items-center gap-x-3 pb-7 pl-4 pt-8">
-          <Image src="/mascot.svg" alt="Mascot" height={40} width={40} />
-
-          <h1 className="text-2xl font-extrabold tracking-wide text-blue-600">
-            YidiOS
-          </h1>
+          <Image src="/YIDIOS.png" alt="Logo" height={40} width={98} />
         </div>
       </Link>
 
