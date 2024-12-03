0ecc52c63f0ed62ad744a7a24ef9e729ae94bbc1
Navya Batra <navyabatra@Navyas-Air.lan>
2024-12-02 23:22:39 -0800

    language courses to game modes

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -13,7 +13,7 @@ const CoursesPage = async () => {
 
   return (
     <div className="mx-auto h-full max-w-[912px] px-3">
-      <h1 className="text-2xl font-bold text-neutral-700">Language Courses</h1>
+      <h1 className="text-2xl font-bold text-neutral-700">Game Modes</h1>
 
       <List courses={courses} activeCourseId={userProgress?.activeCourseId} />
     </div>
