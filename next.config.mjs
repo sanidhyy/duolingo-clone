2863b11f3c5c61664c682fffe1e689389ca5f24c
adelheidriedel <163384836+adelheidriedel@users.noreply.github.com>
2026-01-21 16:35:16 +0000

    Fix deployment and config for Vercel + Clerk

--- next.config.mjs
+++ next.config.mjs
@@ -5,26 +5,15 @@ const nextConfig = {
       {
         source: "/api/(.*)",
         headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-          {
-            key: "Content-Range",
-            value: "bytes : 0-9/*",
-          },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
+          { key: "Content-Range", value: "bytes 0-9/*" },
         ],
       },
     ];
   },
+
   images: {
     remotePatterns: [
       {
@@ -32,6 +21,16 @@ const nextConfig = {
         hostname: "**",
       },
     ],
+  },
+
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+      "https://organic-train-5gvr4jr646v6fp444-3000.app.github.dev",
+      "https://organic-train-5gvr4jr646v6fp444.app.github.dev",
+      "https://*.app.github.dev",
+      ],
+    },
   },
 };
 
