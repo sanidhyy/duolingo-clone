cdc38543f148b627407d2cd68c929a8bf72278ba
Sanidhya Kumar Verma <sanidhyyy@gmail.com>
2026-09-18 16:09:15 +0530

    fix: build

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_API_SECRET_KEY, {
-  apiVersion: "2026-07-29.dahlia",
+  apiVersion: "2026-08-26.dahlia",
   typescript: true,
 });
